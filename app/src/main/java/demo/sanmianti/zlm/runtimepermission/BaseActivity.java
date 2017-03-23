76f3bac9dfa55a39f90dc9dbb669c6abc0239254
周黎明 <zhouliming@up366.com>
2017-03-23 15:16:08 +0800

    优化代码逻辑。

--- app/src/main/java/demo/sanmianti/zlm/runtimepermission/BaseActivity.java
+++ app/src/main/java/demo/sanmianti/zlm/runtimepermission/BaseActivity.java
@@ -23,9 +23,7 @@ public class BaseActivity extends AppCompatActivity {
 
     private RequestPermissionCallBack mRequestPermissionCallBack;
     private final int mRequestCode = 1024;
-    private String mPermissionsName;//权限中文名称，提示用户时用
-
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,6 +40,10 @@ public class BaseActivity extends AppCompatActivity {
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         boolean hasAllGranted = true;
+        StringBuilder  permissionName = new StringBuilder();
+        for (String s: permissions) {
+            permissionName = permissionName.append(s + "\r\n");
+        }
         switch (requestCode) {
             case mRequestCode: {
                 for (int i = 0; i < grantResults.length; ++i) {
@@ -49,10 +51,13 @@ public class BaseActivity extends AppCompatActivity {
                         hasAllGranted = false;
                         //在用户已经拒绝授权的情况下，如果shouldShowRequestPermissionRationale返回false则
                         // 可以推断出用户选择了“不在提示”选项，在这种情况下需要引导用户至设置页手动授权
-                        if (!ActivityCompat.shouldShowRequestPermissionRationale(this, permissions[0])) {
+                        if (!ActivityCompat.shouldShowRequestPermissionRationale(this, permissions[i])) {
                             new AlertDialog.Builder(this)
-                                    .setMessage("用户选择了不在提示按钮，或者系统默认不在提示（如MIUI）。提醒用户：" +
-                                            "获取相关权限失败，将导致部分功能无法正常使用，需要到设置页面手动授权")
+                                    .setMessage("【用户选择了不在提示按钮，或者系统默认不在提示（如MIUI）。" +
+                                            "引导用户到应用设置页去手动授权,注意提示用户具体需要哪些权限】\r\n" +
+                                            "获取相关权限失败:\r\n" +
+                                            permissionName +
+                                            "将导致部分功能无法正常使用，需要到设置页面手动授权")
                                     .setPositiveButton("去授权", new DialogInterface.OnClickListener() {
                                         @Override
                                         public void onClick(DialogInterface dialog, int which) {
@@ -75,13 +80,14 @@ public class BaseActivity extends AppCompatActivity {
                             }).show();
 
                         } else {
+                            //用户拒绝权限请求，但未选中“不再提示”选项
                             mRequestPermissionCallBack.denied();
                         }
                         break;
                     }
-                    if (hasAllGranted) {
-                        mRequestPermissionCallBack.granted();
-                    }
+                }
+                if (hasAllGranted) {
+                    mRequestPermissionCallBack.granted();
                 }
             }
         }
@@ -97,7 +103,10 @@ public class BaseActivity extends AppCompatActivity {
     public void requestPermissions(final Context context, final String[] permissions,
                                    RequestPermissionCallBack callback) {
         this.mRequestPermissionCallBack = callback;
-
+        StringBuilder permissionNames = new StringBuilder();
+        for(String s : permissions){
+            permissionNames = permissionNames.append(s + "\r\n");
+        }
         //如果所有权限都已授权，则直接返回授权成功,只要有一项未授权，则发起权限请求
         boolean isAllGranted = true;
         for (String permission : permissions) {
@@ -106,7 +115,10 @@ public class BaseActivity extends AppCompatActivity {
 
                 if (ActivityCompat.shouldShowRequestPermissionRationale((Activity) context, permission)) {
                     new AlertDialog.Builder(context)
-                            .setMessage("您好，需要相关权限 ，请允许，否则将影响部分功能的正常使用。")
+                            .setMessage("【用户曾经拒绝过你的请求，所以这次发起请求时解释一下】\r\n" +
+                                    "您好，需要如下权限：\r\n" +
+                                    permissionNames+
+                                    " 请允许，否则将影响部分功能的正常使用。")
                             .setPositiveButton("确定", new DialogInterface.OnClickListener() {
                                 @Override
                                 public void onClick(DialogInterface dialog, int which) {
